4da82b5092d2187d7691854691adbeb3a0662923
Artemiy Martyanov <artemiy@martyanov.xyz>
2020-02-14 01:13:06 +0300

    init

---
+++ seed-data.js
@@ -0,0 +1,107 @@
+const axios = require('axios');
+
+let roomNo = 1;
+
+const getRoomType = (sectionIndex, flatIndex) => {
+  switch (sectionIndex + 1) {
+    case 1:
+      switch (flatIndex + 1) {
+        case 1:
+          return '3';
+        case 2:
+          return '1';
+        case 3:
+          return '2';
+        case 4:
+          return '2';
+      }
+    case 2:
+      switch (flatIndex + 1) {
+        case 1:
+          return '1';
+        case 2:
+          return 'C';
+        case 3:
+          return '1';
+        case 4:
+          return '1';
+        case 5:
+          return '2';
+        case 6:
+          return '3';
+        case 7:
+          return '1';
+      }
+    case 3:
+      switch (flatIndex + 1) {
+        case 1:
+          return '3';
+        case 2:
+          return '1';
+        case 3:
+          return '2';
+        case 4:
+          return '2';
+      }
+    case 4:
+      switch (flatIndex + 1) {
+        case 1:
+          return '3';
+        case 2:
+          return '1';
+        case 3:
+          return '2';
+        case 4:
+          return '2';
+      }
+    case 5:
+      switch (flatIndex + 1) {
+        case 1:
+          return '1';
+        case 2:
+          return '3';
+        case 3:
+          return '2';
+        case 4:
+          return '1';
+        case 5:
+          return '1';
+        case 6:
+          return 'C';
+        case 7:
+          return '1';
+      }
+    case 6:
+      switch (flatIndex + 1) {
+        case 1:
+          return '3';
+        case 2:
+          return '1';
+        case 3:
+          return '2';
+        case 4:
+          return '2';
+      }
+  }
+}
+
+[...new Array(6)].forEach(async function (_, sectionIndex) {
+  const isAngle = (sectionIndex + 1) === 2 || (sectionIndex + 1) === 5;
+  const sectionRef = await axios.post('http://localhost:1337/sections', {
+    number: sectionIndex + 1,
+  });
+  [...new Array(isAngle ? 20 : 25)].forEach(async (_, floorIndex) => {
+    const floorRef = await axios.post('http://localhost:1337/floors', {
+      number: floorIndex + 1,
+      section: sectionRef.data.id,
+    });
+    [...new Array(isAngle ? 7 : 4)].forEach(async (_, flatIndex) => {
+      const flatRef = await axios.post('http://localhost:1337/flats', {
+        floor: floorRef.data.id,
+        roomNo: floorIndex === 0 ? '-' : roomNo++,
+        roomType: floorIndex === 0 ? '-' : getRoomType(sectionIndex, flatIndex),
+        contactInfo: '',
+      });
+    });
+  });
+});
