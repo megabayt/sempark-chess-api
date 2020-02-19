7564b48ba8aa11fd5aa9e307eec01bc1e057a8ea
Артемий Мартьянов <artemiy@martyanov.xyz>
2020-02-19 11:16:52 +0300

    move flatFilled to another controller

--- api/flat/controllers/flat.js
+++ api/flat/controllers/flat.js
@@ -6,21 +6,4 @@
  */
 
 module.exports = {
-  findFilled: async ctx => {
-    let result;
-
-    try {
-      result = await strapi.controllers.flat.find(ctx);
-    } catch (err) {
-      strapi.log.error(err);
-      result = [];
-    }
-
-    result = result.map(({ residents, ...flat }) => ({
-      ...flat,
-      hasResident: residents.length > 0,
-    }));
-
-    ctx.send(result)
-  }
 };
